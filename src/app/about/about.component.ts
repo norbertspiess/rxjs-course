c7b37271f1374da27904e87e37b1b777a365301f
Norbert Spieß <norbertspiess@users.noreply.github.com>
2020-12-13 17:56:21 +0100

    basic observable examples

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fromEvent, interval, timer } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -7,9 +8,21 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+
+    // const interval$ = interval(1000); // every x ms
+    // interval$.subscribe(val => console.log('stream 1 => ', val));
+    // interval$.subscribe(val => console.log('stream 2 => ', val));
+
+    // const interval$ = timer(3000, 1000); // every 1000 ms with initial delay of 3000
+    // interval$.subscribe(val => console.log('stream 1 => ', val));
+
+    // const click$ = fromEvent(document, 'click'); // click events on "document"
+    // click$.subscribe(evt => console.log(evt));
+
   }
 
 }
