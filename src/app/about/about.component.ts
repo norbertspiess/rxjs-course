4f47939f1897109e5ba03402ab559cce523df642
Norbert Spieß <norbertspiess@users.noreply.github.com>
2020-12-13 17:56:32 +0100

    kinds of subscriptions and unsubscription

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { fromEvent, interval, timer } from 'rxjs';
+import { fromEvent, timer } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -17,11 +17,17 @@ export class AboutComponent implements OnInit {
     // interval$.subscribe(val => console.log('stream 1 => ', val));
     // interval$.subscribe(val => console.log('stream 2 => ', val));
 
-    // const interval$ = timer(3000, 1000); // every 1000 ms with initial delay of 3000
-    // interval$.subscribe(val => console.log('stream 1 => ', val));
+    const interval$ = timer(3000, 1000); // every 1000 ms with initial delay of 3000
+    const sub = interval$.subscribe(val => console.log('stream 1 => ', val));
 
-    // const click$ = fromEvent(document, 'click'); // click events on "document"
-    // click$.subscribe(evt => console.log(evt));
+    setTimeout(() => sub.unsubscribe(), 5000); // unsubscribe from observable
+
+    const click$ = fromEvent(document, 'click'); // click events on "document"
+    const subscription = click$.subscribe(
+      evt => console.log(evt),
+      err => console.log(err), // either this or completed
+      () => console.log('completed') // either this or error
+    );
 
   }
 
