7b9dde5cb9f42c3ce0b63c3e5353502145c0518a
Norbert Spieß <norbertspiess@users.noreply.github.com>
2020-12-22 15:15:42 +0100

    simple subject to observable example

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,21 +1,31 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public series$: Observable<number>;
 
-    ngOnInit() {
+  ngOnInit() {
 
+    const subject = new Subject<number>(); // keep this one private
+    this.series$ = subject.asObservable(); // this can be public
 
-    }
+    this.series$.subscribe(console.log);
 
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    subject.complete();
+
+    // simple way to create observables, BUT
+    // - no unsubscription logic
+    // - subjects can be misused
+
+  }
 
 }
 
